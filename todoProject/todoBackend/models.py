a6876fdcc4f193a20ea3734872a9cd12175b6512
Wes Alves <wal2.designer@gmail.com>
2022-03-21 22:36:06 +0000

    wip: adding task list view

--- todoProject/todoBackend/models.py
+++ todoProject/todoBackend/models.py
@@ -1,3 +1,10 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField('date published')
+    deadline_date = models.DateField()
+    is_done = models.BooleanField(default=False)
--- todoProject/todoBackend/views.py
+++ todoProject/todoBackend/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import TodoList
+
 # Create your views here.
+def index(request):
+    taskList = TodoList.objects.all()
+
+    return HttpResponse(taskList.name)
+
+def detail(request, TodoList_id):
+    return HttpResponse({"You are looking at this task %s" %TodoList_id})
