31cd75fda6f0479f25657a56440b402cf71ba580
Wes Alves <wal2.designer@gmail.com>
2022-03-22 22:18:51 +0000

    wip: try to get value from input text

--- todoProject/todoBackend/urls.py
+++ todoProject/todoBackend/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'todoBackend'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<TodoList_id>/', views.detail, name='detail')
+    # path('<TodoList_id>/', views.detail, name='detail'),
+    path('<TodoList_id>/', views.edit, name='edit')
 ]
--- todoProject/todoBackend/views.py
+++ todoProject/todoBackend/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import TodoList
+from .forms.taskForm import AddTask
 
 # Create your views here.
 def index(request):
@@ -12,6 +15,16 @@ def index(request):
     return render(request, 'tasks/index.html', context )
 
 def detail(request, TodoList_id):
-    context = {"taskList": ""}
+    task = get_object_or_404(TodoList, pk=TodoList_id)
 
-    return render(request, 'tasks/index.html', context)
+    return render(request, 'tasks/detail.html', {'task':task})
+
+def edit(request, TodoList_id):
+    task = get_object_or_404(TodoList, pk=TodoList_id)
+
+    task.description = request.POST['description.value']
+
+    # task.save()
+
+    return render(request, 'tasks/detail.html', {'task':task})
+    # return HttpResponseRedirect(reverse('todoBackend:edit', args=(task.id,)))
