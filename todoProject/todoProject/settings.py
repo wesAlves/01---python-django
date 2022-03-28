cd71dfeaf7cb94ecb5a7e98b94b81da9e46e2ed8
Wes <wal2.designer@gmail.com>
2022-03-28 22:01:59 +0100

    changes on ubuntu

--- todoProject/todoProject/settings.py
+++ todoProject/todoProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'todoBackend.apps.TodobackendConfig',
     'todoListRestAPI.apps.TodolistrestapiConfig'
 ]
@@ -50,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
 ]
 
 ROOT_URLCONF = 'todoProject.urls'
